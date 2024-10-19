41ab7703e5cda1e9a36884b06b4e227995901a69
dainguyen.nhd <dainguyen.nhd@gmail.com>
2024-10-19 18:09:06 +0700

    fix: list order, apply voucher + buy now feat: context search engine, quantity product & UI product details

--- src/pages/Coupon/CreateCoupon.jsx
+++ src/pages/Coupon/CreateCoupon.jsx
@@ -1,17 +1,17 @@
-import React, { useContext, useState } from "react";
-import DatePicker from "react-datepicker";
-import axios from "axios";
-import "react-datepicker/dist/react-datepicker.css";
-import { CouponContext } from "../../context/CouponContext";
+import React, { useContext, useState } from 'react';
+import DatePicker from 'react-datepicker';
+import axios from 'axios';
+import 'react-datepicker/dist/react-datepicker.css';
+import { CouponContext } from '../../context/CouponContext';
 
 const CreateCoupon = () => {
-  const [couponName, setCouponName] = useState("");
-  const [couponCode, setCouponCode] = useState("");
+  const [couponName, setCouponName] = useState('');
+  const [couponCode, setCouponCode] = useState('');
   const [quantity, setQuantity] = useState(0);
   const [maxUse, setMaxUse] = useState(1);
-  const [discountType, setDiscountType] = useState("fixed");
+  const [discountType, setDiscountType] = useState('fixed');
   const [discountAmount, setDiscountAmount] = useState();
-  const [status, setStatus] = useState("active");
+  const [status, setStatus] = useState('active');
   const [expiryDate, setExpiryDate] = useState(new Date());
   const { addCoupon } = useContext(CouponContext);
 
@@ -19,12 +19,12 @@ const CreateCoupon = () => {
     event.preventDefault();
 
     if (discountAmount < 0) {
-      alert("Discount amount must be greater than or equal to 0.");
+      alert('Discount amount must be greater than or equal to 0.');
       return;
     }
 
     if (maxUse < 1) {
-      alert("Usage limit must be at least 1.");
+      alert('Usage limit must be at least 1.');
       return;
     }
 
@@ -35,35 +35,35 @@ const CreateCoupon = () => {
       expiryDate: expiryDate.toISOString(),
       usageLimit: maxUse,
       status,
-      createdBy: "user12345",
+      createdBy: 'user12345',
     };
 
     try {
-      console.log("New Voucher Data:", newVoucher);
+      console.log('New Voucher Data:', newVoucher);
       const response = await axios.post(
-        "http://localhost:3001/api/v1/vouchers",
+        'http://localhost:3001/api/v1/vouchers',
         newVoucher,
         {
           headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
           },
         }
       );
-      console.log("Created Success ----> ", response.data.data);
+      console.log('Created Success ----> ', response.data.data);
       addCoupon(newVoucher);
 
       // Reset the form
-      setCouponName("");
-      setCouponCode("");
+      setCouponName('');
+      setCouponCode('');
       setQuantity(0);
       setMaxUse(1);
-      setDiscountType("fixed");
+      setDiscountType('fixed');
       setDiscountAmount(0);
-      setStatus("active");
+      setStatus('active');
       setExpiryDate(new Date());
     } catch (error) {
       console.log(
-        "Error creating coupon: ",
+        'Error creating coupon: ',
         error.response ? error.response.data : error.message
       );
     }
@@ -71,76 +71,76 @@ const CreateCoupon = () => {
 
   return (
     <>
-      <div className="max-w-full">
-        <div className="text-4xl m-12 text-center">Create Voucher</div>
-        <div className="max-w-full-lg flex justify-center items-center">
+      <div className='max-w-full'>
+        <div className='text-4xl m-12 text-center'>Create Voucher</div>
+        <div className='max-w-full-lg flex justify-center items-center'>
           <form onSubmit={handleSubmit}>
-            <div className="flex flex-wrap">
-              <div className="mb-8 mr-12">
-                <label className="block mb-4 font-bold">Voucher Code</label>
+            <div className='flex flex-wrap'>
+              <div className='mb-8 mr-12'>
+                <label className='block mb-4 font-bold'>Voucher Code</label>
                 <input
                   value={couponCode}
                   onChange={(e) => setCouponCode(e.target.value)}
-                  className="appearance-none border-2 border-gray-200 rounded w-96 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white"
-                  type="text"
+                  className='appearance-none border-2 border-gray-200 rounded w-96 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white'
+                  type='text'
                   required
                 />
               </div>
             </div>
-            <div className="flex flex-wrap">
-              <div className="mb-8 mr-12">
-                <label className="block mb-4 font-bold">Discount Amount</label>
+            <div className='flex flex-wrap'>
+              <div className='mb-8 mr-12'>
+                <label className='block mb-4 font-bold'>Discount Amount</label>
                 <input
                   value={discountAmount}
                   onChange={(e) => setDiscountAmount(Number(e.target.value))}
-                  className="appearance-none border-2 border-gray-200 rounded w-96 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white"
-                  type="number"
-                  min="0" // Prevent negative numbers
+                  className='appearance-none border-2 border-gray-200 rounded w-96 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white'
+                  type='number'
+                  min='0' // Prevent negative numbers
                   required
                 />
               </div>
-              <div className="mb-8">
-                <label className="block mb-4 font-bold">Max Usage Limit</label>
+              <div className='mb-8'>
+                <label className='block mb-4 font-bold'>Max Usage Limit</label>
                 <input
                   value={maxUse}
                   onChange={(e) => setMaxUse(Number(e.target.value))}
-                  className="appearance-none border-2 border-gray-200 rounded w-96 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white"
-                  type="number"
-                  min="1" // Prevent values less than 1
+                  className='appearance-none border-2 border-gray-200 rounded w-96 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white'
+                  type='number'
+                  min='1' // Prevent values less than 1
                   required
                 />
               </div>
             </div>
-            <div className="flex flex-wrap">
-              <div className="mb-8 mr-12">
-                <label className="block mb-4 font-bold">Expiry Date</label>
+            <div className='flex flex-wrap'>
+              <div className='mb-8 mr-12'>
+                <label className='block mb-4 font-bold'>Expiry Date</label>
                 <DatePicker
-                  className="border border-gray-200 w-96 p-2 focus:outline-none focus:bg-white"
+                  className='border border-gray-200 w-96 p-2 focus:outline-none focus:bg-white'
                   selected={expiryDate}
                   onChange={(date) => setExpiryDate(date)}
                   required
                 />
               </div>
             </div>
-            <div className="flex flex-wrap">
-              <div className="mb-8">
-                <label className="block mb-4 font-bold">Status</label>
+            <div className='flex flex-wrap'>
+              <div className='mb-8'>
+                <label className='block mb-4 font-bold'>Status</label>
                 <select
                   value={status}
                   onChange={(e) => setStatus(e.target.value)}
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
                 >
-                  <option value="active">Active</option>
-                  <option value="expired">Expired</option>
-                  <option value="used">Used</option>
-                  <option value="disabled">Disabled</option>
+                  <option value='active'>Active</option>
+                  <option value='expired'>Expired</option>
+                  <option value='used'>Used</option>
+                  <option value='disabled'>Disabled</option>
                 </select>
               </div>
             </div>
 
             <button
-              type="submit"
-              className="border border-gray-500 w-56 h-10 float-end hover:bg-black hover:text-white"
+              type='submit'
+              className='border border-gray-500 w-56 h-10 float-end hover:bg-black hover:text-white'
             >
               Create
             </button>
--- src/pages/Product/ListProduct.jsx
+++ src/pages/Product/ListProduct.jsx
@@ -1,52 +1,81 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import Loading from '../../components/Loading';
+import SearchContext from '../../context/SearchContext';
+import logo from '../../assets/react.svg';
 
-const ProductList = () => {
-    const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const navigate = useNavigate();
+const ListProduct = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const { searchQuery } = useContext(SearchContext);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchProducts = async () => {
-            try {
-                const response = await axios.get('http://localhost:3001/api/v1/products');
-                setProducts(response.data.data);
-                setLoading(false);
-            } catch (err) {
-                setError('Failed to fetch products');
-                setLoading(false);
-            }
-        };
-
-        fetchProducts();
-    }, []);
-
-    const handleProductClick = (id) => {
-        navigate(`/product/details/${id}`);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get(
+          'http://localhost:3001/api/v1/products'
+        );
+        setProducts(response.data.data);
+      } catch (err) {
+        setError('Failed to fetch products');
+      } finally {
+        setLoading(false);
+      }
     };
 
-    if (loading) return <Loading />;
-    if (error) return <div>{error}</div>;
+    fetchProducts();
+  }, []);
 
-    return (
-        <div className="flex flex-wrap">
-            {products.map((product) => (
-                <div key={product._id} className="m-auto" onClick={() => handleProductClick(product._id)}>
-                    <div className="m-2 cursor-pointer">
-                        <img src={product.createdBy.avatarUrl} alt={product.name} className="w-full" />
-                        <h3><strong>Product Name: </strong>{product.name}</h3>
-                        <p>Description: {product.previewDescription}</p>
-                        <p><strong>Price:</strong> ${product.price}</p>
-                        <p><strong>Stock:</strong> {product.stock}</p>
-                        <p><strong>Created by:</strong> {product.createdBy.fullName}</p>
-                    </div>
-                </div>
-            ))}
+  const handleProductClick = (id) => {
+    navigate(`/product/details/${id}`);
+  };
+
+  const filteredProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  if (loading) return <Loading />;
+  if (error) return <div>{error}</div>;
+  if (!filteredProducts.length) return <div>No products available.</div>;
+
+  return (
+    <div className='flex flex-wrap'>
+      {filteredProducts.map((product) => (
+        <div
+          key={product._id}
+          className='m-auto'
+          onClick={() => handleProductClick(product._id)}
+        >
+          <div className='m-2 cursor-pointer'>
+            <img src={logo} alt={product.name} className='w-full' />
+            <h3>
+              <strong>Product Name: </strong>
+              {product.name}
+            </h3>
+            <p>Description: {product.previewDescription}</p>
+            <p>
+              <strong>Price:</strong> ${product.price}
+            </p>
+            <p>
+              <strong>Stock:</strong> {product.stock}
+            </p>
+            <p className='flex items-center'>
+              <strong>Created by: </strong>
+              <img
+                src={product.createdBy.avatarUrl}
+                className='w-10 rounded-full mx-2'
+                alt=''
+              />{' '}
+              {product.createdBy.fullName}
+            </p>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
-export default ProductList;
+export default ListProduct;
